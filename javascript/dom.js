50d5b805e986fd164d5463987896e38eabde2a9a
razibmahmud50 <razibmahmud50@gmail.com>
2020-01-27 12:36:43 +0600

    update

---
+++ javascript/dom.js
@@ -0,0 +1,26 @@
+// selector
+document.getElementById('')
+document.querySelector('')
+document.querySelectorAll('')
+document.querySelector('').getAttribute('')
+document.querySelector('').value
+document.querySelector('').innerText
+document.querySelector('').innerHTML
+document.querySelector('').childrens//only immidiate child
+document.querySelector('').nextElementSibling //only one immidiate next
+document.querySelector('').previousElementSibling //only one immidiate prev
+document.querySelector('').classList.contains()//if contain specific class name
+
+//create and set method
+document.querySelector('').attributesName == "attributesValue"
+document.querySelector('').css({})
+document.createElement('tagName')//create HTML tag
+document.appendChild('childHere')
+document.createTextNode("Water");//create plain text
+
+
+
+
+
+
+
